2e6052da28d8d1efcfa29711588f81f4d40c3171
TheLegend207 <HannTham20@gmail.com>
2026-07-02 18:44:50 +0800

    Created Player Movement
    created player controller script
    created player animation
    Mario is now controllable/movable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 3f;
+    public float jumpForce = 8f;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    public object thePlayer;
+    public Rigidbody2D rb;
+    private float moveInput;
+    private bool isGrounded;
+    private bool isFacingRight = true;
+    private Animator myAnim;
+
+    // public LevelManager theLevelManager; (tba)
+    public bool canMove = true;
+
     void Start()
     {
-        
+        thePlayer = FindAnyObjectByType<PlayerController>();
+        rb = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput*moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        if (moveInput > 0 && !isFacingRight)
+            Flip();
+        else if (moveInput < 0 && isFacingRight)
+            Flip();
+        myAnim.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
     }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            whatIsGround
+        );
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+void OnTriggerEnter2D(Collider2D other)
+    {
+      if (other.tag == "KillPlane")
+        {
+            // thePlayer.SetActive(false);
+        }
+    }
+
 }

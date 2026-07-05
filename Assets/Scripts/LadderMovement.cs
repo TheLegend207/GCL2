19274a6ac788df4247d07a77c18cfc29eee727c2
Sanjeev Shanmugasundaram <sanje6080j@gmail.com>
2026-07-05 10:56:30 +0800

    Made ladders climbable
    mario can climb ladders now

---
+++ Assets/Scripts/LadderMovement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+         private float vertical;
+        private float speed = 3f;
+        private bool isLadder;
+        private bool isClimbing;
+
+
+        [SerializeField] private Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (isLadder && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+            rb.gravityScale = 0f;
+        }
+        else if (!isLadder || Mathf.Abs(vertical) == 0f)
+        {
+            isClimbing = false;
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+            rb.gravityScale = 1f;
+        }
+    }
+}
+
+
